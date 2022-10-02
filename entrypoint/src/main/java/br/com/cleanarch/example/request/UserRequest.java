b0ccbb3f83baf87b3a8541761e113c44526a91bb
tapantoniopedroso <tap.antonio.pedroso@gmail.com>
2022-10-01 22:10:35 -0300

    unit tests post implementation

--- entrypoint/src/main/java/br/com/cleanarch/example/request/UserRequest.java
+++ entrypoint/src/main/java/br/com/cleanarch/example/request/UserRequest.java
@@ -1,8 +1,12 @@
 package br.com.cleanarch.example.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
     private String name;
 }
--- entrypoint/src/test/java/br/com/cleanarch/example/UserControllerTest.java
+++ entrypoint/src/test/java/br/com/cleanarch/example/UserControllerTest.java
@@ -2,6 +2,7 @@ package br.com.cleanarch.example;
 
 import br.com.cleanarch.example.converter.UserRequestConverter;
 import br.com.cleanarch.example.create.controller.UserController;
+import br.com.cleanarch.example.request.UserRequest;
 import br.com.cleanarch.example.request.UserResponse;
 import br.com.cleanarch.example.user.contract.UserService;
 import br.com.cleanarch.example.user.entity.User;
@@ -11,30 +12,31 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class UserControllerTest {
 
     @Mock private UserService userService;
 
+
     @BeforeEach void setUp(){
         User user = new User("awsd2-sde4454feedef-454554", "Thiago Pedroso");
+
+        Mockito.when(userService.save(Mockito.any(User.class))).thenReturn(user.getId());
         Mockito.when(userService.find(user.getId())).thenReturn(user);
-//        Mockito.when(userService.save(user)).thenReturn(user.getId());
     }
 
     @Test
     public void shouldGetUserControllerStatusCode200Return(){
+
         String id = "awsd2-sde4454feedef-454554";
-
         var controler = new UserController(userService);
-
         var response = controler.getUser(id);
 
         assertThat(response.getStatusCodeValue()).isEqualTo(HttpStatus.OK.value());
@@ -44,12 +46,30 @@ public class UserControllerTest {
     public void shouldGetUserControllerBodyUserNameReturn(){
         String id = "awsd2-sde4454feedef-454554";
         var userResponse = new UserResponse("Thiago Pedroso");
-
         var controler = new UserController(userService);
-
         var response = controler.getUser(id);
 
         assertThat(response.getBody()).isEqualTo(userResponse);
+
+    }
+    @Test
+    public void shouldPostUserControllerAndStatus20Return(){
+
+        var userRequest = new UserRequest("Thiago Pedroso");
+        var controler = new UserController(userService);
+        var response = controler.save(userRequest);
+
+        assertThat(response.getStatusCodeValue()).isEqualTo(HttpStatus.OK.value());
+
+    }
+    @Test
+    public void shouldPostUserControllerAndIdReturn(){
+        String id = "awsd2-sde4454feedef-454554";
+        var userRequest = new UserRequest("Thiago Pedroso");
+        var controler = new UserController(userService);
+        var response = controler.save(userRequest);
+
+        assertThat(response.getBody()).isEqualTo(String.format("{\"id\":%s}", id));
 
     }
 }
